5ce484aaf0ded7e80e0002e36d95af7ef7e66682
Jeremy Doucet <jeremy.doucet93@gmail.com>
2015-11-10 10:09:09 -0800

    Full CRUD

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -24,7 +24,7 @@ function displayMarkers() {
 displayMarkers();
 
 //C - Create
-function addPen() {
+function addMarker() {
   //Grab the the inputs from our HTML
   var color = document.getElementById('inputColor');
   var material = document.getElementById('inputMat');
@@ -44,6 +44,36 @@ function addPen() {
   document.getElementById('markerResults').innerHTML += displayMarker(marker);
 }
 
+// U - Update
+function editMarker(i) {
+  //Change the input values to be this specific marker
+  document.getElementById('inputColor').value = markerArray[i].color;
+  document.getElementById('inputMat').value = markerArray[i].material;
+  document.getElementById('inputPerm').value = markerArray[i].isPerm;
+
+  //Overwrite the submit button in the form, with edit buttons
+  document.getElementById('Button-Group').innerHTML = '<button class="btn btn-success" onclick="saveMarker(' + i + ')">Save</button><button class="btn btn-danger" onclick="clearInputs()">Cancel</button>'
+}
+
+function saveMarker(i) {
+  //Get the values from the inputs
+  var color = document.getElementById('inputColor').value;
+  var mat = document.getElementById('inputMat').value;
+  var perm = document.getElementById('inputPerm').value;
+
+  //create a new marker based on the values
+  var marker = new Marker(color, mat, perm);
+
+  //Replace the old marker in the array
+  markerArray[i] = marker;
+
+  //Redraw the submit button, and clear the inputs
+  clearInputs();
+
+  //Redraw all of the markers
+  displayMarkers();
+}
+
 // D - Delete
 function deleteMarker(i) {
   //Remove the selected marker from the array
@@ -54,5 +84,15 @@ function deleteMarker(i) {
 
 //Display an individual marker
 function displayMarker(marker) {
-  return "<li><i>Color</i>: " + marker.color + ", <i>Material</i>: " + marker.material + ", <i>Is permanent</i>: " + marker.isPerm + "<button class='btn btn-danger' onclick='deleteMarker(" + markerArray.indexOf(marker) + ")'>&times;</button></li>";
+  return "<li><i>Color</i>: " + marker.color + ", <i>Material</i>: " + marker.material + ", <i>Is permanent</i>: " + marker.isPerm + "<button class='btn btn-warning' onclick='editMarker(" + markerArray.indexOf(marker) + ")'>Edit</button><button class='btn btn-danger' onclick='deleteMarker(" + markerArray.indexOf(marker) + ")'>&times;</button></li>";
+}
+
+function clearInputs() {
+  //Clear out all of the inputs
+  document.getElementById('inputColor').value = "";
+  document.getElementById('inputMat').value = "";
+  document.getElementById('inputPerm').value = "true";
+
+  //Overwrite the edit buttons with a save button
+  document.getElementById('Button-Group').innerHTML = '<button type="button" class="btn btn-success" onclick="addMarker()">Submit</button>'
 }
